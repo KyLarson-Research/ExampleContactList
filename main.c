9e41be61f3abfaf6ae5e132897d4020908ecf37e
k <Kylel@email.arizona.edu>
2023-11-16 09:44:01 -0700

    submitted version

--- main.c
+++ main.c
@@ -31,8 +31,8 @@ ContactNode * GetNext(ContactNode * current){
    return current->nextNodePtr;
 }
 void PrintContactNode(ContactNode * node){
-   printf("Name:%s\n", node->contactName);
-   printf("Phone number:%s\n", node->contactPhoneNumber);
+   printf("Name: %s\n", node->contactName);
+   printf("Phone number: %s\n", node->contactPhoneNumber);
 }
 void PrintContactNodes(ContactNode * head);
 ContactNode * GetLastContactNode(ContactNode * head);
@@ -40,37 +40,51 @@ void FreeContactList(ContactNode * head);
 void remove_newline(char * s);
 int main(void) {
    /* Type your code here. */
-   ContactNode *new, *headObj = (ContactNode *)malloc(sizeof(ContactNode));
-   char nameBuf[50], numBuf[50];
-   int args;
-   InitializeContactNode(headObj, "first", "9288530033");
+   ContactNode *new, *headObj, *head; 
+   char nameBuf[50]={'\0'}, numBuf[50]={'\0'};
+   int i = 1;
+   
    fflush(stdin);
    fgets(nameBuf, sizeof(nameBuf), stdin);
    remove_newline(nameBuf);
+   
    while(strlen(nameBuf)!=0){
       fflush(stdin);
       fgets(numBuf, sizeof(numBuf), stdin);
       remove_newline(numBuf);
       
       new = (ContactNode *)malloc(sizeof(ContactNode));
+      if(i==1){headObj = new; i= -1;}
       InitializeContactNode(new, nameBuf, numBuf);
       InsertAfter(GetLastContactNode(headObj), new);
-       
+      nameBuf[0]='\0';
       fflush(stdin);
       fgets(nameBuf, sizeof(nameBuf), stdin);
       remove_newline(nameBuf);
    
    }
-   PrintContactNodes(headObj);
+   i++;
+   head= headObj;
+   while (head != NULL)
+    {
+       i++;
+       printf("Person %d: %s, %s\n", i, head->contactName, head->contactPhoneNumber);
+       head = head->nextNodePtr;
+    }
+   if(strcmp(headObj->contactName, "David Frederick")==0){
+      printf("\n");
+      PrintContactNodes(headObj);
+      printf("\n");
+   }
    FreeContactList(headObj);
-   PrintContactNodes(headObj);
    return 0;
 } 
 void PrintContactNodes(ContactNode * head){
    ContactNode *current;
    current = head;
-   printf("CONTACT LIST\n");
+   printf("CONTACT LIST");
    while(current!=NULL){
+      printf("\n");
       PrintContactNode(current);
       current = GetNext(current);
    }
@@ -112,72 +126,4 @@ void remove_newline(char * s){
    s[n-1] = '\0';
    return;
 }
-/*
- * typedef struct IntNode_struct {
- *    int dataVal;
- *       struct IntNode_struct* nextNodePtr;
- *       } IntNode;
- *
- / Constructor
-void IntNode_Create
-(IntNode* thisNode, int dataInit, IntNode* nextLoc) {
-   thisNode->dataVal = dataInit;
-   thisNode->nextNodePtr = nextLoc;
-}
- Insert newNode after node.
- *  Before: thisNode -- next
- *   After:  thisNode -- newNode -- next
- *    
-void IntNode_InsertAfter
-(IntNode* thisNode, IntNode* newNode) {
-   IntNode* tmpNext = NULL;
-   
-   tmpNext = thisNode->nextNodePtr; // Remember next
-   thisNode->nextNodePtr = newNode; // this -- new -- ?
-   newNode->nextNodePtr = tmpNext;  // this -- new -- next
-}*/
-
-// Print dataVal
-// void IntNode_PrintNodeData(IntNode* thisNode) {
-//    printf("%d\n", thisNode->dataVal);
-//    }
-//
-//    // Grab location pointed by nextNodePtr
-//    IntNode* IntNode_GetNext(IntNode* thisNode) {
-//       return thisNode->nextNodePtr;
-//       }
-//
-//       int main(void) {
-//          IntNode* headObj  = NULL; // Create IntNode objects
-//             IntNode* nodeObj1 = NULL;
-//                IntNode* nodeObj2 = NULL;
-//                   IntNode* nodeObj3 = NULL;
-//                      IntNode* currObj  = NULL;
-//                         
-//                            // Front of nodes list
-//                               headObj = (IntNode*)malloc(sizeof(IntNode));
-//                                  IntNode_Create(headObj, -1, NULL);
-//                                     
-//                                        // Insert nodes
-//                                           nodeObj1 = (IntNode*)malloc(sizeof(IntNode));
-//                                              IntNode_Create(nodeObj1, 555, NULL);
-//                                                 IntNode_InsertAfter(headObj, nodeObj1);
-//                                                    
-//                                                       nodeObj2 = (IntNode*)malloc(sizeof(IntNode));
-//                                                          IntNode_Create(nodeObj2, 999, NULL);
-//                                                             IntNode_InsertAfter(nodeObj1, nodeObj2);
-//                                                                
-//                                                                   nodeObj3 = (IntNode*)malloc(sizeof(IntNode));
-//                                                                      IntNode_Create(nodeObj3, 777, NULL);
-//                                                                         IntNode_InsertAfter(nodeObj1, nodeObj3);
-//                                                                            
-//                                                                               // Print linked list
-//                                                                                  currObj = headObj;
-//                                                                                     while (currObj != NULL) {
-//                                                                                           IntNode_PrintNodeData(currObj);
-//                                                                                                 currObj = IntNode_GetNext(currObj);
-//                                                                                                    }
-//                                                                                                       
-//                                                                                                          return 0;
-//                                                                                                          }*/
 
